918a4f4272ca21fb77fd7b29cfba590fa66b0c9c
pc <pc@DESKTOP-0E1LEKR>
2022-12-01 19:26:46 +0530

    Removed the space in Demo class

--- src/main/java/com/sample/Demo.java
+++ src/main/java/com/sample/Demo.java
@@ -4,8 +4,12 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("GitHUB learning ");
-		
 		System.out.println("updated the code");
+		
+		
+		
+		
+		
 		
 	}
 
